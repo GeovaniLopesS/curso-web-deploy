6822d27f7ec2360e78ec9667372863a52870c194
Geovani Lopes Sampaio <77645237+geovanisampaio13@users.noreply.github.com>
2023-10-18 17:19:44 -0400

    deploy

---
+++ src/pages/general/Home/Componente7.tsx
@@ -0,0 +1,55 @@
+import {style} from "@pages/general/Home/index.tsx";
+
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Button } from 'primereact/button';
+import React, {useState} from 'react';
+
+import { Slider, SliderChangeEvent } from "primereact/slider";
+import { AutoComplete, AutoCompleteCompleteEvent } from 'primereact/autocomplete';
+import { Knob, KnobChangeEvent } from 'primereact/knob';
+
+export const Componente7 = () => {
+    const [value, setValue] = useState(0);
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
+    const [value3, setValue3] = useState('');
+        
+    const [box, setBox] = useState(0);
+    const [knob, setKnob] = useState(0);
+
+    const [items, setItems] = useState<string[]>([]);
+    const search = (event: AutoCompleteCompleteEvent) => {
+        setItems([...Array(10).keys()].map(item => event.query + '-' + item));
+    }
+      
+    return <div className={`w-full h-screen flex flex-row m-2 p-4`}>
+
+        <div className={style}>
+            <div className="flex-column w-full">
+            <InputTextarea value={value3} onChange={(e) => {
+                //@ts-ignore
+                setValue3(e.target.value)}} rows={5} cols={30} />
+            <Slider value={value1} onChange={(e) => {
+                // @ts-ignore
+                setValue1(e.value)}} className="w-14rem" />
+            <Knob value={value2} onChange={(e) => {
+                //@ts-ignore
+                setValue2(e.value)}}/>
+            </div>
+            <div>
+                <Button icon="pi pi-check" rounded
+                />
+            </div>
+        </div>
+
+        <div className={style}>
+        <div className="card flex justify-content-center flex-column align-items-center ">
+                <AutoComplete value={box} suggestions={items} completeMethod={search} onChange={(f) => setBox(f.value)} className='m-2'/>
+                <Slider value={value} onChange={(e: SliderChangeEvent) => {
+                    // @ts-ignore
+                    setValue(e.value)}} className="w-14rem m-4" />
+                <Knob value={knob} onChange={(g: KnobChangeEvent) => setKnob(g.value)} />
+            </div>
+        </div>
+    </div>
+}
